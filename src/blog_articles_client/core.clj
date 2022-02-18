63ee360887dc850c80d62912781056d978d0fcdf
i-whammy <yas.iwami@gmail.com>
2022-02-18 21:05:41 +0900

    pick up status

--- src/blog_articles_client/core.clj
+++ src/blog_articles_client/core.clj
@@ -5,5 +5,5 @@
   (let [companies (-> (http/get "http://localhost:3000/companies"
                                 {:as :json})
                       (get-in [:body :companies]))]
-    (map #(http/put "http://localhost:3000/articles"
-                    {:query-params {:company %}}) companies)))
+    (map #(:status (http/put "http://localhost:3000/articles"
+                    {:query-params {:company %}})) companies)))
