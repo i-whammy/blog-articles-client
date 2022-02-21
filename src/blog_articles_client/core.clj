d4cc8737efb2a5cf84727be625c336a03878745e
i-whammy <yas.iwami@gmail.com>
2022-02-21 20:25:32 +0900

    enable debug

--- src/blog_articles_client/core.clj
+++ src/blog_articles_client/core.clj
@@ -1,9 +1,18 @@
 (ns blog-articles-client.core
   (:require [clj-http.client :as http]))
 
-(defn execute []
-  (let [companies (-> (http/get "http://localhost:3000/companies"
+(defn put-company [company]
+  (try
+    (http/put "http://localhost:3000/articles"
+                    {:query-params {:company company}})
+    (catch Exception _
+           (println (str "company = " company)))))
+
+(defn- get-companies []
+  (-> (http/get "http://localhost:3000/companies"
                                 {:as :json})
-                      (get-in [:body :companies]))]
-    (map #(:status (http/put "http://localhost:3000/articles"
-                    {:query-params {:company %}})) companies)))
+                      (get-in [:body :companies])))
+
+(defn execute []
+  (let [companies (get-companies)]
+    (map #(:status (put-company %)) companies)))
