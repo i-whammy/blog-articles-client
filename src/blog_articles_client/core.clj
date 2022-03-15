2d0ea9870e35a71aa0cd61f23253b4f9fd3144c5
i-whammy <yas.iwami@gmail.com>
2022-03-15 19:19:02 +0900

    fix error message

--- src/blog_articles_client/core.clj
+++ src/blog_articles_client/core.clj
@@ -4,15 +4,18 @@
 (defn put-company [company]
   (try
     (http/put "http://localhost:3000/articles"
-                    {:query-params {:company company}})
+              {:query-params {:company company}})
     (catch Exception _
-           (println (str "company = " company)))))
+      (println (str "Error occured. company = " company)))))
 
 (defn- get-companies []
   (-> (http/get "http://localhost:3000/companies"
-                                {:as :json})
-                      (get-in [:body :companies])))
+                {:as :json})
+      (get-in [:body :companies])))
 
 (defn execute []
   (let [companies (get-companies)]
-    (map (fn [company] {:status (:status (put-company company)) :company company}) companies)))
+    (map (fn [company]
+           {:status (:status (put-company company))
+            :company company})
+         companies)))
